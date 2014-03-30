80ddf6fd721058696502f9bc7d00104f190c61c5
vic <git@dfhu.org>
2014-03-30 00:00:15 -0400

    a self contained mitm proxy and test that uses simplehtmlserver that does not hang and also injects

--- tests/test-hunky.py
+++ tests/test-hunky.py
@@ -2,9 +2,13 @@ import sys
 from os.path import dirname, realpath
 sys.path.append(dirname(dirname(realpath(__file__))))
 
+import SimpleHTTPServer
+import SocketServer
+import os
+
+from threading import Thread
 from urllib2 import urlopen
 from CollectAllProxy import CollectAllProxy
-#from httplib import BadStatusLine
 from time import sleep
 
 
@@ -16,12 +20,36 @@ def shutdown_thread(t):
     print "done shutting down"
 
 
+class Httpd(Thread):
+    httpd = None
+
+    def __init__(self):
+        super(Httpd, self).__init__()
+
+        PORT = 8000
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+        self.httpd = SocketServer.TCPServer(("", PORT), Handler)
+        print "serving at port", PORT
+
+    def run(self):
+        self.httpd.serve_forever()
+
+    def shutdown(self):
+        self.httpd.shutdown()
+
+
 if __name__ == '__main__':
+    os.chdir(dirname(dirname(realpath(__file__))) + "/html")
 
     inport = "8777"
+    http_port = "8000"
+
+    httpd = Httpd()
+    httpd.start()
+
     cap = CollectAllProxy(
         '127.0.0.1', int(inport),
-        '127.0.0.1', 8000)
+        '127.0.0.1', int(http_port))
     print "Starting Cap"
     cap.start()
     print "Cap Running"
@@ -32,13 +60,19 @@ if __name__ == '__main__':
 
     response = urlopen('http://127.0.0.1:' + inport)
     print "**From server**"
-    print response.read()
+    response1 = response.read()
+    print response1
     print "**Done reading**"
 
     response = urlopen('http://127.0.0.1:' + inport)
     print "from server 2"
-    print response.read()
+    response2 = response.read()
+    print response2
     print "Done reading 2"
 
     print "Shutting Down"
     shutdown_thread(cap)
+    shutdown_thread(httpd)
+
+    assert response1.find("INJECTED!")
+    assert response2.find("INJECTED!")
